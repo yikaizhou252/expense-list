c5eed53b629d656e7313b945e12cd6feb4f3a5f3
Yikai Zhou <zhou.yikai76@gmail.com>
2023-01-02 11:59:09 -0500

    Added event filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,24 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
-  return <Card className="expenses">{renderExpenses(props.expenses)}</Card>;
+  const [selectedYear, setSelectedYear] = useState(2023);
+
+  const yearSelectHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter currentYear={selectedYear} onSelectYear={yearSelectHandler} />
+        {renderExpenses(props.expenses)}
+      </Card>
+    </div>
+  );
 };
 
 const renderExpenses = (expenses) => {
